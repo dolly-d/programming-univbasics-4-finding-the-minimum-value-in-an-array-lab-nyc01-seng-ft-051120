7f01cc3b8cb9d9f0c73cc907de023b84517c9a10
Dolly Desir <dolly.desir@gmail.com>
2020-05-03 20:22:27 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -4,6 +4,8 @@ def find_min_value(array)
     while count < array.length do
       if min_value > array[count]
         min_value = array[count]
+         end
+      count += 1
     end
     min_value
 end
